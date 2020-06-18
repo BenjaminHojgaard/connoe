066bc9241cfd80436010dc64f133bc1996c5e001
BenjaminHojgaard <benjaminoest@hotmail.com>
2020-06-18 16:34:44 +0200

    Initial commit

---
+++ ConnOe.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ConnOe.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ ConnOe.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ConnOe.Services
+{
+    public class Class1
+    {
+    }
+}
