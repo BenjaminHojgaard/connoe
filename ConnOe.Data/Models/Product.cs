21944985eca4fffc1cc89d44af00f025e00a5caf
BenjaminHojgaard <benjaminoest@hotmail.com>
2020-06-20 20:26:40 +0200

    Add entity models

---
+++ ConnOe.Data/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnOe.Data.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsTaxable { get; set; }
+        public bool IsArchived { get; set; }
+    }
+}
